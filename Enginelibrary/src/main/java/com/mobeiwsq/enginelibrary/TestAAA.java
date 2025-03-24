f5463a5b17fb14eb1ca58430c3a4a9a1f86eae5c
sunwn <2650164725@qq.com>
2025-03-24 11:47:32 +0800

    项目初始化

---
+++ Enginelibrary/src/main/java/com/mobeiwsq/enginelibrary/TestAAA.java
@@ -0,0 +1,10 @@
+package com.mobeiwsq.enginelibrary;
+
+import android.util.Log;
+
+public class TestAAA {
+
+    public static void testAA(){
+        Log.d("swn_mmmmm", "testAA: 1111111111111111111111111111");
+    }
+}
