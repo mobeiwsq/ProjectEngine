f637943e54820dab631596dd3a536842fdd5a87b
sunwn <2650164725@qq.com>
2025-04-24 17:02:18 +0800

    没用使用openpage打开第一个activity时，返回键失效

--- app/src/main/java/com/mobeiwsq/projectengine/MainActivity.kt
+++ app/src/main/java/com/mobeiwsq/projectengine/MainActivity.kt
@@ -3,28 +3,12 @@ package com.mobeiwsq.projectengine
 import android.os.Bundle
 import com.mobeiwsq.engine_project.engine.base.EngineActivity
 import com.mobeiwsq.engine_project.engine.core.openPage
-
-
-//class MainActivity : EngineDataBindActivity<ActivityMainBinding>(R.layout.activity_main) {
-//
-//    override fun initView() {
-//    }
-//
-//    override fun initData() {
-//    }
-//
-//    override fun initListeners() {
-//        Log.d("AAAAAA", "Button reference: ${binding.button}")
-//        binding.button.setOnClickListener {
-//            Log.d("AAAAAA", "initListeners: 11111111")
-//        }
-//    }
-//
-//}
+import com.mobeiwsq.projectengine.view.fragment.MainFragment
 
 class MainActivity : EngineActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        openPage(MainFragment::class.java)
+//        openPage(MainFragment::class.java)
     }
 }
