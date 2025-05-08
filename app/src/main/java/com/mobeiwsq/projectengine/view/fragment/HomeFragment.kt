544ff4b3ba698c1b0a4da3dd4f0993eedd69c224
sunwn <2650164725@qq.com>
2025-05-08 20:48:14 +0800

    集成日历工具

--- app/src/main/java/com/mobeiwsq/projectengine/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/mobeiwsq/projectengine/view/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import com.mobeiwsq.engine_project.adapter.SimpleAdapter
 import com.mobeiwsq.engine_project.adapter.simple.AdapterItem
 import com.mobeiwsq.engine_project.engine.base.EngineFragment
 import com.mobeiwsq.engine_project.engine.core.openPage
+import com.mobeiwsq.engine_project.view.calendarview.Calendar
 import com.mobeiwsq.engine_project.view.title.TitleBar
 import com.mobeiwsq.engine_project.view.widget.popupwindow.SimplePopup
 import com.mobeiwsq.projectengine.R
@@ -19,6 +20,16 @@ class HomeFragment : EngineFragment<FragmentHomeBinding>(R.layout.fragment_home)
     }
 
     override fun initData() {
+        val map: MutableMap<String, Calendar> = HashMap()
+        map[getSchemeCalendar(2025, 1, 13, -0xec5310, "假").toString()] =
+            getSchemeCalendar(2025, 1, 13, -0xec5310, "卡")
+        map[getSchemeCalendar(2025, 1, 12, -0xec5310, "卡").toString()] =
+            getSchemeCalendar(2025, 1, 12, -0xec5310, "卡")
+        map[getSchemeCalendar(2025, 1, 11, -0xec5310, "卡").toString()] =
+            getSchemeCalendar(2025, 1, 11, -0xec5310, "卡")
+        map[getSchemeCalendar(2025, 1, 1, -0xec5310, "卡").toString()] =
+            getSchemeCalendar(2025, 1, 1, -0xec5310, "卡")
+        binding.calendarView.setSchemeDate(map)
     }
 
     var dpiItems: Array<String> = arrayOf(
@@ -55,6 +66,19 @@ class HomeFragment : EngineFragment<FragmentHomeBinding>(R.layout.fragment_home)
 
     override fun initTitleBar(): TitleBar? {
         return null
+    }
+
+    private fun getSchemeCalendar(year: Int, month: Int, day: Int, color: Int, text: String): Calendar {
+        val calendar = Calendar()
+        calendar.year = year
+        calendar.month = month
+        calendar.day = day
+        calendar.schemeColor = color //如果单独标记颜色、则会使用这个颜色
+        calendar.scheme = text
+        calendar.addScheme(Calendar.Scheme())
+        calendar.addScheme(-0xff7800, "假")
+        calendar.addScheme(-0xff7800, "节")
+        return calendar
     }
 
 
